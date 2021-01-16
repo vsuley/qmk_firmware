6f9dfaa662aab8f894cf029d4d1bf759c56234d0
Vinayak Suley <vsuley@hotmail.com>
2021-01-15 16:06:37 -0800

    Minor improvements

--- keyboards/crkbd/keymaps/vsuley/keymap.c
+++ keyboards/crkbd/keymaps/vsuley/keymap.c
@@ -118,16 +118,16 @@ void oled_render_layer_state(void) {
             oled_write_ln_P(PSTR("Colemak"), false);
             break;
         case L_LOWER:
-            oled_write_ln_P(PSTR("Lower"), false);
+            oled_write_ln_P(PSTR("Numbers"), false);
             break;
         case L_RAISE:
-            oled_write_ln_P(PSTR("Raise"), false);
+            oled_write_ln_P(PSTR("Symbols"), false);
             break;
         case L_ADJUST:
         case L_ADJUST|L_LOWER:
         case L_ADJUST|L_RAISE:
         case L_ADJUST|L_LOWER|L_RAISE:
-            oled_write_ln_P(PSTR("Adjust"), false);
+            oled_write_ln_P(PSTR("System"), false);
             break;
     }
 }
